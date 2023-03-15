fa9a0f3fafca65890862a7300cf1b9e09db61f60
youjin <w2117@e-mirim.hs.kr>
2023-03-15 09:58:34 +0900

    arrouw function 기본

--- 문법.js
+++ 문법.js
@@ -9,17 +9,24 @@
 
 
 // 함수
-function add(n1, n2) {
-    return n1 + n2;
-}
-result = add(1, 2); // 3
-console.log(result); // 3
+// function add(n1, n2) {
+//     return n1 + n2;
+// }
+// result = add(1, 2); // 3
+// console.log(result); // 3
 
-plus = add; //plus도 add()의 역할을 할 수 있음
-result = plus(3, 4); // 7
-console.log(result); // 7
+// plus = add; //plus도 add()의 역할을 할 수 있음
+// result = plus(3, 4); // 7
+// console.log(result); // 7
 
-sub = function(n1, n2) {
+// sub = function(n1, n2) {
+//         return n1 - n2;
+//     } // 익명함수
+// result = sub(3, 2); // 1
+// console.log(result); // 1
+
+
+sub = (n1, n2) => {
         return n1 - n2;
     } // 익명함수
 result = sub(3, 2); // 1
