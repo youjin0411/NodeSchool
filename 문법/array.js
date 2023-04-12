3a15b995d13327990397dbcbfcfd241d0cec859d
youjin <w2117@e-mirim.hs.kr>
2023-04-12 10:17:14 +0900

    array.indexOf()

--- 문법/array.js
+++ 문법/array.js
@@ -6,7 +6,17 @@ arr.forEach((data, index) => {
 });
 
 //return 값이 true에 해당하는 index를 반환.
-const cherryIndex = arr.findIndex((data) => {
-    return data === 'cherry';
-})
-console.log(cherryIndex);
+const cherryIndex = arr.findIndex(data => data === 'cherry');
+// console.log(cherryIndex);
+
+// 3번 index부터 'cherry'에 해당하는 index를 찾음, 없으면 -1 리턴 
+const a = arr.indexOf('cherry', 3);
+console.log(a);
+
+// 배열 끝 부분에 data 추가
+arr.push('orange');
+console.log(arr);
+
+const x = arr.pop();
+// arr.push();
+// console.log(arr);
