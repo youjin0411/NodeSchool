79d9168909c90d58a2e2d9577c3166ebdb36d3bf
youjin <w2117@e-mirim.hs.kr>
2023-04-13 14:04:29 +0900

    getDuplicated() 디렉토리 점검

--- 사진관리/main.js
+++ 사진관리/main.js
@@ -20,5 +20,32 @@ function getAllFiles(dirPath, arrOfFiles) {
     });
     return arrOfFiles;
 }
-const files = getAllFiles(__dirname + "\\base", []);
-console.log(files.join('\n'));
+
+function getDuplicated(baseDir) {
+    //existsSync: 해당 경로에 파일이나 폴더가 존재하는지 확인
+    //파일이 존재하는지 유효성 검사 
+    if (!fs.existsSync(baseDir)) {
+        console.log("폴더가 존재하지 않음.");
+        return;
+    }
+
+    const duplicatedDir = path.join(baseDir, 'duplicated');
+    console.log(duplicatedDir);
+    // 폴더가 존재하지 않으면 폴더 생성
+    if (!fs.existsSync(duplicatedDir)) {
+        // mkdirSync: 폴더 생성
+        fs.mkdirSync(duplicatedDir);
+    }
+    // [] 없으면 에러 발생
+    // 그 이유는 getAllFiles 함수에서 arrOfFiles를 초기화 하지 않았기 때문
+    // base 폴더에 있는 모든 파일들의 목록(list)
+    const arrayOfFiles = getAllFiles(baseDir, []);
+    // 중복된 파일들(절대 경로) list
+    const duplicatedFiles = [];
+}
+
+// const files = getAllFiles(__dirname + "\\base", []);
+// const files = getAllFiles(path.join(__dirname, 'base'), []);
+// console.log(files.join('\n'));
+
+getDuplicated(path.join(__dirname, 'base'));
